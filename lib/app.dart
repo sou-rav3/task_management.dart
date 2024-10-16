5b8431f10d706c9cce84f92f9209d10fda735f89
sou-rav3 <mdsourav54u@gmail.com>
2024-10-16 23:15:40 +0600

    updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:task_management/ui/screens/splash_screen.dart';
 
-class TaskManagerApp extends StatelessWidget {
+class TaskManagerApp extends StatefulWidget {
   const TaskManagerApp({super.key});
 
   @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        inputDecorationTheme: _inputDecorationTheme(),
         textTheme: const TextTheme(),
       ),
       home: const SplashScreen(),
     );
+  }
+
+  InputDecorationTheme _inputDecorationTheme(){
+    return InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      border: _inputBorder(),
+      enabledBorder: _inputBorder(),
+      errorBorder: _inputBorder(),
+      focusedBorder: _inputBorder(),
+    );
+  }
+
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(8));
   }
 }
--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_management/ui/utils/app_colors.dart';
 import 'package:task_management/ui/widgets/screen_background.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -19,7 +20,7 @@ class _SignInScreenState extends State<SignInScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 111),
+              const SizedBox(height: 151),
               Text(
                 'Get Stated With',
                 style: textTheme.displaySmall
@@ -27,24 +28,28 @@ class _SignInScreenState extends State<SignInScreen> {
               ),
               const SizedBox(height: 22),
               TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(8)),
+                decoration: const InputDecoration(
+                  hintText: 'Email',
                 ),
               ),
               const SizedBox(height: 22),
               TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(8)),
-                ),
+                decoration: const InputDecoration(hintText: 'Password'),
               ),
+              const SizedBox(height: 82),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.themeColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 22, vertical: 13),
+                    fixedSize: const Size.fromWidth(double.maxFinite),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Icon(Icons.arrow_forward_ios)),
             ],
           ),
         ),
